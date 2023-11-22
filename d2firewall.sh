be3b32b9a368b1001f81ccf97f6e23db4b048ffd
xmaver <84945775+xmaver@users.noreply.github.com>
2023-11-22 00:28:29 -0300

    Update d2firewall.sh

--- d2firewall.sh
+++ d2firewall.sh
@@ -281,7 +281,24 @@ elif [ "$action" == "sniff" ]; then
     read -t 1 -n 1
     if [ $? = 0 ] ; then
       break
-    fi
+    fi;
+  elif [ "$action" == "sniff steam" ]; then
+    platform=$(sed -n '1p' < data.txt)
+       exit 1
+  fi
+  bash d2firewall.sh -a stop
+
+  #auto sniff
+  echo -e "${RED}Press any key to stop sniffing. DO NOT CTRL C${NC}"
+
+  sleep 1
+    ngrep -l -q -W byline -d $INTERFACE "steamid:" udp | grep --line-buffered -o -P 'steamid:[0-9]{7}\K[0-9]{10}' | tee -a data.txt &
+  fi
+  while [ true ] ; do
+    read -t 1 -n 1
+    if [ $? = 0 ] ; then
+     break
+      fi
   done
   pkill -15 ngrep
 
